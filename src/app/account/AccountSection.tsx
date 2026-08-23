eb6ed383c49b460ec5e3b5f6eb6b353e98c284d3
ulianzs1805 <ulianazeonova1805@gmail.com>
2026-08-24 00:21:58 +0300

    fix: repair AccountSection production build callback type

--- src/app/account/AccountSection.tsx
+++ src/app/account/AccountSection.tsx
@@ -79,6 +79,6 @@ export default function AccountSection({ section }: { section: Section }) {
     {section === "transactions" && <TransactionsSection transactions={data.transactions} />}
     {section === "statistics" && <StatisticsSection statistics={statistics} bestDrop={bestDrop} />}
     {section === "settings" && <SettingsSection user={data.user} onResetBeta={() => void resetBetaAccess()} />}
-    {section === "support" && <SupportTicketPanel tickets={data.tickets} onRefresh={load} />}
+    {section === "support" && <SupportTicketPanel tickets={data.tickets} onRefresh={() => void load()} />}
   </AccountShell>;
 }
