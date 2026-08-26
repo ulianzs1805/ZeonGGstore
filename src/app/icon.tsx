024fc171b7297ec474831108ccda722999cfbffc
ulianzs1805 <ulianazeonova1805@gmail.com>
2026-08-26 21:41:35 +0300

    Fix app icon logo path to uploaded PNG

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -3,6 +3,8 @@ import { ImageResponse } from "next/og";
 export const size = { width: 192, height: 192 };
 export const contentType = "image/png";
 
+const LOGO_URL = "https://raw.githubusercontent.com/ulianzs1805/ZeonGGstore/terminal/public/AFED0327-AB02-4E23-9FCC-B94940A08A4C.png";
+
 export default function Icon() {
   return new ImageResponse(
     (
@@ -19,7 +21,7 @@ export default function Icon() {
         }}
       >
         <img
-          src="https://raw.githubusercontent.com/ulianzs1805/ZeonGGstore/terminal/public/ZeonGgstore-logo.png"
+          src={LOGO_URL}
           width="192"
           height="192"
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
